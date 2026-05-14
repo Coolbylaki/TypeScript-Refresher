485e61697b65787e20fc6e58540fba22b1eacd8c
Lazar Stojanović <lakisd98@gmail.com>
2026-05-14 03:25:10 +0200

    ADD: Helper log function with types

--- FetchJson/index.ts
+++ FetchJson/index.ts
@@ -9,16 +9,19 @@ interface Todo {
 }
 
 axios.get(url).then((response) => {
-	// console.log(response.data);
 	const todo: Todo = response.data;
 
 	const ID = todo.id;
 	const title = todo.title;
-	const finished = todo.completed;
+	const completed = todo.completed;
 
-	console.log(`
-    The Todo with ID: ${ID}
-    Has a title of: ${title}
-    Is it finished? ${finished}
-    `);
+	logTodo(ID, title, completed);
 });
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+	console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
+    `);
+};
