7fcdfd15fc938c78d14563f5bc2d1724d28d26c2
Lazar Stojanović <lakisd98@gmail.com>
2026-05-18 07:26:06 +0200

    Add: void and never examples

--- Features/Annotations/functions.ts
+++ Features/Annotations/functions.ts
@@ -13,3 +13,19 @@ function divide(a: number, b: number) {
 const multiply = function (a: number, b: number) {
 	return a * b;
 };
+
+const logger = (msg: string): void => {
+	console.log("🚀 ~ logger ~ msg:", msg);
+
+	// return "You cannot return anything in void"
+};
+
+const throwError = (msg: string) => {
+	if (!msg) {
+		throw new Error(msg);
+	}
+
+	return msg;
+
+	// return "Error will never reach past that point so it will never return anything"
+};
