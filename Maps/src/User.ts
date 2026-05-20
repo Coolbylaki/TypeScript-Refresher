912cc6fe61f4d9adc412ebdce28ecc325e161486
Lazar Stojanović <lakisd98@gmail.com>
2026-05-20 02:37:25 +0200

    Add: company class with fake data

--- Maps/src/User.ts
+++ Maps/src/User.ts
@@ -4,21 +4,14 @@ export class User {
 	name: string;
 	location: {
 		lat: number;
-		long: number;
+		lng: number;
 	};
 
 	constructor() {
 		this.name = faker.person.firstName();
 		this.location = {
 			lat: faker.location.latitude(),
-			long: faker.location.longitude(),
+			lng: faker.location.longitude(),
 		};
-	}
-
-	// This is a test
-	print() {
-		console.log(
-			`My name is ${this.name} and I am at ${this.location.lat}, ${this.location.long} coordinates`,
-		);
 	}
 }
